1fe7695dfee53d1f2252696b555028cadb9c9805
irfanariq <irfanariqzaki@gmail.com>
2017-10-16 20:36:37 +0000

    edit receiver

--- receiver.cpp
+++ receiver.cpp
@@ -3,12 +3,17 @@
 #include <unistd.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+#include "sendframe.h"
+#include "frameack.h"
 
 using namespace std;
 
+// GLOBAL
 struct sockaddr_in serverAddr;
 struct sockaddr_storage serverStorage;
 socklen_t addr_size;
+
+int udpSocket;
 
 void configureSetting(int portNum) {
 	serverAddr.sin_family = AF_INET;
@@ -17,39 +22,48 @@ void configureSetting(int portNum) {
 	memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
 } 
 
-int main(int argc, char* argv[]){
-	cout << "hello world\n\n";
+void sendACK(int seqNumber, int adWindowSize) {
+	FrameAck frameack(seqNumber, adWindowSize);
+	int msgLength = frameack.getFrameNumber();
+	sendto(udpSocket, frameack.toBytes(), 6, 0, (struct sockaddr*) &serverAddr, addr_size);
+	cout << "Frame number " << frameack.getNextSeqNumber() <<" sent" << endl; 
+}
 
+int main(int argc, char* argv[]){
+
+	int buffersize = 100;
 	int WINDOW_SIZE = 5;
 	addr_size = sizeof serverStorage;
 
 	int portNum = 9876;
 	char msg[100];
-
-
+	
 	// Membuat UDP socket
-    int udpSocket = socket(PF_INET, SOCK_DGRAM, 0);
-    // cout << "Membuat socket untuk koneksi ke " << "1" << ":" << portNum << endl;
-    // configureSetting("1", portNum);
-
+    udpSocket = socket(PF_INET, SOCK_DGRAM, 0);   
     configureSetting(portNum);
 	int hasilBind = bind(udpSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
-	cout << "buat baru" << endl;
-	
 	cout << hasilBind << endl;
-	int i = 0;
 
+	// ----------------------------------------------------
+
+	SendFrame * recvMesg;
+	recvMesg = new SendFrame[buffersize];
+
+	int i = 0;
 	int check;
 
 	while(1) {
 		cout << "wait " << i << endl;
-		i++;
 
 		check = recvfrom(udpSocket,msg,9,0,(struct sockaddr *)&serverStorage, &addr_size);
-		bitset<8> bit(msg[6]);
-		char data = msg[6];
-		cout << bit << endl;
-		cout << data << endl;
+		
+		unsigned int seq = (msg[1]<<24) + (msg[2]<<16) + (msg[3] << 8) + msg[4];
+		recvMesg[i] = SendFrame(msg[6], seq);
+
+		cout << "Sequence Number : " << recvMesg[i].getSeqNumber() << endl;
+		cout << "Data : " << recvMesg[i].getData() << endl;
+
+		i++;
 	}
 
 	close(udpSocket);
